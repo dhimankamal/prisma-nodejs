caa31475c761e51e9bb37e3bf585c022131fca1b
kamal <dhimank079@gmail.com>
2022-11-03 19:54:11 +0530

    update data

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const { PrismaClient } = require('@prisma/client')
+const { updatePostData,updateCategoryData,updateTagsData } = require('./lib')
 
 const prisma = new PrismaClient()
 
-async function main() {
-    const allUsers = await prisma.data.findMany()
-    console.log(allUsers)
+//functions
+const main = async () => {
+ // let updatePostDataRes = await updatePostData()
+ // let updateCategoryDataRes = await updateCategoryData()
+  let updateTagsDataRes = await updateTagsData()
+  console.log('updateCategoryDataRes++', updateTagsDataRes)
 }
 
 main()
   .then(async () => {
     await prisma.$disconnect()
   })
-  .catch(async (e) => {
+  .catch(async e => {
     console.error(e)
     await prisma.$disconnect()
     process.exit(1)
